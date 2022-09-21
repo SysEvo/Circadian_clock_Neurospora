14566b0b426394818fafa969d6cfe3841672cdb5
Emma00000 <60990020+Emma00000@users.noreply.github.com>
2022-09-21 13:29:57 -0500

    Update modelo_complejo.m
    New model with more interactions and some changes into the equations.

--- modelo_complejo.m
+++ modelo_complejo.m
@@ -1,9 +1,10 @@
 function y = modelo_complejo(tm,x)
-%Parametros
-    k = [1.19, 90, 1.6, 0.03, 0.226, 2.4, 1, 0.472, 0.3, 0.001, 1, 5, 0.001, 7.3, 320, 5.4, 0.15, 2, 0.05, 800, 0.68, 0.3];
+%Parametros    
+    k = [1.19, 1.2, 90, 1.6, 0.03, 0.226, 2.4, 1, 0.472, 0.3, 0.001, 50, 5, 0.001, 7.3, 320, 5.4, 0.15, 2, 0.05, 800, 0.68, 0.3];
     n = 2;
-    K = [1, 0.03, 0.3, 0.5, 5, 1.8, 0.18, 0.02];
-    kd = [2.4, 2.5, 0.135, 0.085, 0.05, 0.05, 6, 0.23, 0.69, 0.34, 0.34, 0.1, 6.2, 0.24, 0.24];
+    K = [0.3, 0.5, 5, 1.8, 0.18, 0.02];
+    kd = [2.4, 2.5, 0.135, 0.085, 0.05, 0.05, 0.02, 0.23, 0.69, 0.34, 0.34, 0.1, 6.2, 0.24, 0.24];
+    
     l = [0, 0.2];
 %mRNAs y Proteinas
     wc1 = x(1);
@@ -15,11 +16,11 @@ function y = modelo_complejo(tm,x)
     laWCC = x(7);
     frq = x(8);
     FRQ = x(9);
-    FRQ_FRH = x(10);
-    FRQp = x(11);
-    FRQn = x(12);
+    FFC = x(10);
+    FFCp = x(11);
+    FFCn = x(12);
     vvd = x(13);
-    VVD = x(14);
+    VVDc = x(14);
     VVDn = x(15);
 %Tiempo y periodos LD
     m=mod(tm,24);
@@ -30,24 +31,24 @@ function y = modelo_complejo(tm,x)
     end
     
 %Ecuaciones
-    dwc1 = k(1) + k(2)*laWCC^n/(K(1)^n+laWCC^n) - kd(1)*wc1;
-    dwc2 = k(3)/(K(2) + WCCn) + k(4)*FRQn - kd(2)*wc2;
-    dWC1 = k(5)*wc1 + k(6)*(FRQp^n/(K(3)^n + FRQp^n))*wc1 - kd(3)*WC1 - k(7)*WC1*WC2;
-    dWC2 = k(8)*wc2 - kd(4)*WC2 - k(7)*WC1*WC2;
-    dWCCc = k(7)*WC1*WC2 - kd(5)*WCCc - k(9)*WCCc;
-    dWCCn = k(9)*WCCc + k(10)*laWCC - kd(6)*WCCn - k(11)*(FRQn^n/(K(4)^n + FRQn^n))*WCCn - L*WCCn;
-    dlaWCC = L*WCCn - kd(7)*laWCC - k(12)*(VVDn^n/(K(5)^n + VVDn^n))*laWCC - k(13)*laWCC;
-    dfrq = k(14)*WCCn^n/(K(6)^n + WCCn^n) + k(15)*laWCC^n/(K(7)^n + laWCC^n) - kd(8)*frq;
-    dFRQ = k(16)*frq - kd(9)*FRQ - k(17)*FRQ;
-    dFRQ_FRH = k(17)*FRQ - kd(10)*FRQ_FRH - k(18)*FRQ_FRH;
-    dFRQp = k(18)*FRQ_FRH - kd(11)*FRQp - k(19)*FRQp;
-    dFRQn = k(19)*FRQp - kd(12)*FRQn;
-    dvvd = k(20)*laWCC^n/(K(8)^n + laWCC^n) - kd(13)*vvd;
-    dVVD = k(21)*vvd - kd(14)*VVD - k(22)*VVD;
-    dVVDn = k(22)*VVD - kd(15)*VVDn;
+    dwc1 = k(1) + k(2)*WCCn + k(3)*laWCC - kd(1)*wc1;
+    dwc2 = k(4)/(1 + WCCn) + k(5)*FFCn - kd(2)*wc2;
+    dWC1 = k(6)*wc1 + k(7)*(FFCp^n/(K(1)^n + FFCp^n))*wc1 - kd(3)*WC1 - k(9)*WC1*WC2;
+    dWC2 = k(8)*wc2 - kd(4)*WC2 - k(9)*WC1*WC2;
+    dWCCc = k(9)*WC1*WC2 - kd(5)*WCCc - k(10)*WCCc;
+    dWCCn = k(10)*WCCc + k(11)*laWCC - kd(6)*WCCn - k(12)*(FFCn^n/(K(2)^n + FFCn^n))*WCCn - L*WCCn;
+    dlaWCC = L*WCCn - kd(7)*laWCC - k(13)*(VVDn^n/(K(3)^n + VVDn^n))*laWCC - k(14)*laWCC;
+    dfrq = k(15)*WCCn^n/(K(4)^n + WCCn^n) + k(16)*laWCC^n/(K(5)^n + laWCC^n) - kd(8)*frq;
+    dFRQ = k(17)*frq - kd(9)*FRQ - k(18)*FRQ;
+    dFFC = k(18)*FRQ - kd(10)*FFC - k(19)*FFC;
+    dFFCp = k(19)*FFC - kd(11)*FFCp - k(20)*FFCp;
+    dFFCn = k(20)*FFCp - kd(12)*FFCn;
+    dvvd = k(21)*laWCC^n/(K(6)^n + laWCC^n) - kd(13)*vvd;
+    dVVDc = k(22)*vvd - kd(14)*VVD - k(23)*VVD;
+    dVVDn = k(23)*VVD - kd(15)*VVDn;
 
 
 
-    y= [dwc1; dwc2; dWC1; dWC2; dWCCc; dWCCn; dlaWCC; dfrq; dFRQ; dFRQ_FRH; dFRQp; dFRQn; dvvd; dVVD; dVVDn];
+    y= [dwc1; dwc2; dWC1; dWC2; dWCCc; dWCCn; dlaWCC; dfrq; dFRQ; dFFC; dFFCp; dFFCn; dvvd; dVVD; dVVDn];
 
 end
