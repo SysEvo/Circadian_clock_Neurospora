9f265735053c19a1dfdd2c4f88be43ac24aebb7a
Emma00000 <emmanuel_231220@outlook.com>
2022-10-13 09:57:12 -0500

    Change of the code to run it in the cluster

--- Simulacion_st.m
+++ Simulacion_st.m
@@ -1,28 +1,11 @@
-clear; clc;
-
 % Simulate stochastic dynamics:
-T = zeros(1000,1);  % Simulated time points
-X = zeros(1000,15);  % Simulated states (size # of species)
-
 t = 0;              % Current (initial) time point
 x = ones(1,15)*10;       % Current (initial) state (size # of species)
 
-tMAX = 200;         % Maximum simulation time
+tMAX = 100;         % Maximum simulation time
 
-i = 0;
-
-while(t<tMAX)
-    i = i + 1;
-    X(i,:) = x;
-    T(i)   = t;
-    i = i + 1;
-    X(i,:) = x;
-    [t,x] = modelo_st(t , x );
-    T(i)   = t;
-end
-
-clear i t x
-
+y = parfeval(@Gillespie, 2, x, t, tMAX);
+[T,X] = fetchOutputs(y);
 % Plot stochastic dynamics
 
 ax1=nexttile;
@@ -39,3 +22,22 @@ xlabel("Tiempo")
 ylabel("Concentracion")
 xlim([100, 200])
 legend("FFCn")
+
+%Save
+save("Data_st.txt", "X", "T", "-ascii")
+
+%Function of the Gillespie algorithm.
+function [T,X] = Gillespie(x,t,tMAX )
+    T = zeros(1000,1);  % Simulated time points
+    X = zeros(1000,15);  % Simulated states (size # of species)
+    i = 0;
+    while(t<tMAX)
+        i = i + 1;
+        X(i,:) = x;
+        T(i)   = t;
+        i = i + 1;
+        X(i,:) = x;
+        [t,x] = modelo_st(t , x );
+        T(i)   = t;
+    end
+end
