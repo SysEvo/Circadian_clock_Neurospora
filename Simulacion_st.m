ddc17bef306ad113ec3da93d562220d456eb4004
Emma00000 <emmanuel_231220@outlook.com>
2022-11-16 10:17:21 -0600

    Adjustment

--- Simulacion_st.m
+++ Simulacion_st.m
@@ -1,54 +1,28 @@
 % Simulate stochastic dynamics:
 t = 0;              % Current (initial) time point
-
-x = [1200,2,1966,8,10,50,24,22,95,32,29,42,2030,3186,6648];       % Current (initial) state (size # of species)
+x = [12700,15819,496,332515,8851,1338,1,99,491,25,103,248,0,12,1607];       % Current (initial) state (size # of species)
 %x=ones(1,15);
-tMAX = 320;         % Maximum simulation time
+tMAX = 264;         % Maximum simulation time
 prompt="Name file of save:";
 file=input(prompt, 's');
 
 
 [T,X] = Gillespie(x,t,tMAX,file);
-% Plot stochastic dynamics
-
-% Plot stochastic dynamics
-
-%plot(T,X(:,[8 12]),'-','MarkerSize',3)
-%xlabel("Tiempo")
-%ylabel("Concentracion")
-%title("LD n=3")
-%xlim([100, 200])
-%legend(["frq","FFCn"])
-
-%ax1=nexttile;
-%plot(ax1,T,X(:,8),'-','MarkerSize',3)
-%title("LD 12:12")
-%xlabel("Tiempo")
-%ylabel("Concentracion")
-%xlim([100, 200])
-%legend("frq")
-
-%ax2=nexttile;
-%plot(ax2,T,X(:,12),'-','MarkerSize',3)
-%xlabel("Tiempo")
-%ylabel("Concentracion")
-%xlim([100, 200])
-%legend("FFCn")
 
 %Function of the Gillespie algorithm.
 function [T,X] = Gillespie(x,t,tMAX ,file)
-    T = zeros(6*10^7,1);  % Simulated time points
-    X = zeros(6*10^7,15);  % Simulated states (size # of species)
+    T = zeros(3*10^8,1);  % Simulated time points
+    X = zeros(3*10^8,15);  % Simulated states (size # of species)
     i = 0;
-    ranum1=rand(5*10^7,1);
-    ranum2=rand(5*10^7,1);    
+    ranum1=rand(2*10^8,1);
+    ranum2=rand(2*10^8,1);    
     while(t<tMAX)
         i = i + 1;
         X(i,:) = x;
         T(i)   = t;
         i = i + 1;
         X(i,:) = x;
-	[t,x] = modelo_st(t , x , 144,ranum1(i/2),ranum2(i/2))
+	[t,x] = modelo_st(t , x , 120,ranum1(i/2),ranum2(i/2));
         T(i)   = t;
         m=mod(t,12);
         if m>11.999
@@ -56,4 +30,8 @@ function [T,X] = Gillespie(x,t,tMAX ,file)
             save("Data_st1.mat", "X", "T","-v7.3")
         end
     end
+    z=find(T,1,"last");
+    T=T(1:z);
+    X=X(1:z,:);
+    save(file, "X", "T","-v7.3")
 end
