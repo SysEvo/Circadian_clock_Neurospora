7e8ff918be42da686ccfcfb1901e1dda56909c22
Emma00000 <emmanuel_231220@outlook.com>
2023-04-21 13:45:30 -0600

    Correction of selection

--- Simulacion_compleja.m
+++ Simulacion_compleja.m
@@ -1,23 +1,23 @@
 clear; close all; clc;
 
 x0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];   %initial values of concentration of the molecules.
-tM = 0:0.05:450;                                      %time to the simulation
+tM = 0:0.05:800;                                      %time to the simulation
 [t,x] = ode45(@modelo_complejo,tM,x0);        %Solve the ODE system, x contains the concentration of the molecules at t time.
 
 
 % Plot ODE results
-[pks1,locs1] = findpeaks(x(:,8));   %To localize the maximus points.
-[pks2,locs2] = findpeaks(x(:,12));
+%[pks1,locs1] = findpeaks(x(:,8));   %To localize the maximus points.
+%[pks2,locs2] = findpeaks(x(:,12));
 
-plot(t,x(:,[7,8,12]),'-')
-text(t(locs1)+.01,pks1,num2str(t(locs1)),"FontSize",6)
-text(t(locs2)+.05,pks2,num2str(t(locs2)),"FontSize",5)
+plot(t,x(:,6),'-')
+%text(t(locs1)+.01,pks1,num2str(t(locs1)),"FontSize",6)
+%text(t(locs2)+.05,pks2,num2str(t(locs2)),"FontSize",5)
 title("LD DD 3.0")
 xlabel("Tiempo")
 ylabel("Concentracion")
-xlim([100,450])
+xlim([100,800])
 %ylim([2.2,2.4])
-legend(["laWCC","frq","FRQn"])
+%legend(["laWCC","frq","FRQn"])
 
 %Plot ODE results to compare molecules with different scale of concentration.
 %ax1=nexttile;
